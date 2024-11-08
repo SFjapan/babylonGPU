76b8fdb40ba5facee84845e7675b6bcfd742290d
SFjapan <fs12265669@gmail.com>
2024-11-08 12:25:15 +0900

    tokenを使用してmodel_countやmodel_indexが変更された際、作成中のモデルを 中断して再作成する

--- src/gl.ts
+++ src/gl.ts
@@ -5,16 +5,14 @@ import { HemisphericLight } from '@babylonjs/core/Lights/hemisphericLight';
 import { Vector3 } from '@babylonjs/core/Maths/math.vector';
 import { Color3 } from '@babylonjs/core/Maths/math.color';
 import { CreateGround } from '@babylonjs/core/Meshes/Builders/groundBuilder';
-import { CreateSphere } from '@babylonjs/core/Meshes/Builders/sphereBuilder';
 import { StandardMaterial } from '@babylonjs/core/Materials/standardMaterial';
-import { Collider, PrecisionDate, SceneLoader, UniversalCamera, WebGPUEngine } from '@babylonjs/core';
-import "@babylonjs/loaders"
-import { ChangeEvent } from 'react';
-
+import { PrecisionDate, SceneLoader, UniversalCamera, WebGPUEngine } from '@babylonjs/core';
+import "@babylonjs/loaders";
+import { models } from './models';
 
 window.addEventListener('DOMContentLoaded', async() => {
     const startCPU = PrecisionDate.Now;
-
+    
     const gl_canvas = document.getElementById('canvas')as unknown as HTMLCanvasElement; // キャンバスの取得
     let fps = document.getElementById('fps'); //fps表示
     const engine = new Engine(gl_canvas); // BABYLONエンジンの初期化
@@ -55,7 +53,7 @@ window.addEventListener('DOMContentLoaded', async() => {
             for(let z = 0; z < 13;z++){
                 const model = await SceneLoader.ImportMeshAsync("","./models/","house.glb",scene,null,null,"house");
                 let house = model.meshes[0];
-                house.id = "house";
+                house.id = "model";
                 house.position = new Vector3((x*15)+0,0,(z*15)+0);
                 house.scaling = new Vector3(10,10,10);
             }
@@ -67,35 +65,76 @@ window.addEventListener('DOMContentLoaded', async() => {
 
     const scene = await createScene(); // シーンの作成
     let duration = 0;
+    const loading = document.getElementById("loading");
+    let scene_loaded = false;
     scene.executeWhenReady(()=>{
         const endCPU = PrecisionDate.Now;
         duration = (endCPU - startCPU) / 1000;
+        loading.style.display = "none";
+        scene_loaded = true;
     });
     engine.runRenderLoop(() => {
         scene.render();
         const current_fps = Math.floor(engine.getFps());
         fps.innerHTML ="fps:" +  current_fps +  "," +  duration.toFixed(2) + "秒";
     });
-    const house_count = document.getElementById('house-count') as HTMLInputElement;
-    house_count.addEventListener('change',async (e:Event)=>{
-        const target = e.target as HTMLInputElement;
-        const value = target.value as unknown as number;
-        target.nextElementSibling.innerHTML = "house-count:" + value;
-        scene.getMeshesById("house").forEach(house=>{
-            house.dispose();
-        })
-        for(let x = 0; x < value;x++){
-            for(let z = 0; z < value;z++){
-                const model = await SceneLoader.ImportMeshAsync("","./models/","cat.glb",scene,null,null,"house");
-                let mesh = model.meshes[0];
-                mesh.id = "house";
-                mesh.position = new Vector3((x*15)+0,0,(z*15)+0);
-                mesh.rotation = new Vector3(0,90,0);
-                mesh.scaling = new Vector3(10,10,10);
-            }
-        }
-    })
     window.addEventListener('resize', () => {
         engine.resize();
     });
+
+   
+    //input navigations
+    let model_index = 0;
+    let model_count = 10;
+    const house_count = document.getElementById('model-count') as HTMLInputElement;
+    const select_model = document.getElementById('select-model') as HTMLSelectElement;
+    let cansellationToken :{cancelled:boolean} | null = null;
+    house_count.addEventListener('change',async (e:Event)=>{
+        const target = e.target as HTMLInputElement;
+        const value = target.value as unknown as number;
+        target.nextElementSibling.innerHTML = "model-count:" + value;
+        model_count = value;
+        await restatModelCreation();
+    });
+
+    select_model.addEventListener('change',async (e:Event)=>{
+        const target = e.target as HTMLInputElement;
+        const value = target.value as unknown as number;
+        model_index = value;
+        await restatModelCreation();
+    });
+
+    async function restatModelCreation(){
+        if(!scene_loaded)return;
+        //トークンがあったらtrueにして渡す
+        if(cansellationToken){
+            cansellationToken.cancelled = true;
+        }
+        //新しくトークンをfalseで作る
+        cansellationToken = {cancelled:false};
+        await createModel(models[model_index].model,model_count,models[model_index].offset,cansellationToken);
+    }
+
+    async function createModel(model:string,count:number,offset:number,token:{cancelled:boolean}){
+        //メッシュをすべて削除して作成
+        scene.getMeshesById("model").forEach(mesh => mesh.dispose());
+
+        for(let x = 0; x < count;x++){
+            for(let z = 0; z < count;z++){
+                if(token.cancelled){
+                    //途中でトークンのcancellがtrueになったらメッシュをすべて削除してreturn
+                    scene.getMeshesById("model").forEach(mesh => mesh.dispose());
+                    return;
+                }
+                const current_model = await SceneLoader.ImportMeshAsync("","./models/",model+ ".glb",scene,null,null,"house");
+                let mesh = current_model.meshes[0];
+                mesh.id = "model";
+                mesh.position = new Vector3((x*offset)+0,0,(z*offset)+0);
+                mesh.scaling = new Vector3(10,10,10);
+            }
+        }
+    }
+    
+    
+   
 });
---
+++ src/models.ts
@@ -0,0 +1,10 @@
+export const models = [
+    {
+        model:"house",
+        offset:15,
+    },
+    {
+        model:"cat",
+        offset:5
+    }
+];
